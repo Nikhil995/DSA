0e2462bd6b1ef97ceca5b839087effbf433c0388
Nikhil agrawal <99258446+Nikhil995@users.noreply.github.com>
2023-04-10 20:28:49 +0530

    Add files via upload

--- Binary Search Tree/min_value.java
+++ Binary Search Tree/min_value.java
@@ -1,1 +1,48 @@
+package binary_tree;
 
+
+
+class Tree{
+    int key;
+    Tree left;
+    Tree right;
+    Tree(int data){
+        key=data;
+    }
+}
+
+public class min_value {
+    static Tree insert(Tree root,int data){
+        if(root==null){
+            return new Tree(data);
+        }
+        if(data<root.key){
+           return root.left=insert(root.left,data);
+
+        }
+        return root.right=insert(root.right,data);
+    }
+
+    static int  min(Tree root){
+        if(root==null){
+            return -1;
+        }
+        if(root.left==null){
+            return root.key;
+        }
+        return min(root.left);
+    }
+    public static void main(String[] args) {
+        Tree root=null;
+        root=insert(root,200);
+        insert(root,50);
+        insert(root,100);
+        insert(root,150);
+        insert(root,250);
+        insert(root,225);
+        insert(root,25);
+        System.out.println(min(root));
+
+    }
+    
+}
